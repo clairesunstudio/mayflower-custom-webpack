8520ad7e7e4ca3ffdb336155dbb214a9bd5167d4
Minghua Sun <clairesunstudio@gmail.com>
2020-02-06 14:26:26 -0500

    test icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Button } from "@massds/mayflower-react";
+import { Button, Icon } from "@massds/mayflower-react";
 
 const App = () => (
   <main>
     <h1>Hello world</h1>
     <Button text="Start" />
+    <Icon name="alert" svgHeight={40} svgWidth={40} />
   </main>
 );
 
